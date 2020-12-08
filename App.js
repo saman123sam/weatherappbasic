ba909b6865ea4ca5cffb7f63c71a01ae8c05828c
Aman Maharjan <mhrjn.aman@gmail.com>
2020-12-08 12:09:19 +0545

    Intro to react navigation

--- App.js
+++ App.js
@@ -1,23 +1,60 @@
+import React from "react";
+import { StyleSheet, View, SafeAreaView, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import Header from './components/Header';
+import Header from "./components/Header";
 
-export default class App extends Component{
-    render(){
-        return(
-          <SafeAreaView style={{flex: 1}}>
-            <View style={styles.container}>
-            <Header/>
-            </View>
-            </SafeAreaView>
-        )
-    }
-}
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+  const handleTextPress = () => navigation.navigate("Detail");
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Header />
+
+        <View style={{ alignItems: "center", marginTop: 20 }}>
+          <Text onPress={handleTextPress} style={{ fontSize: 16 }}>
+            View Detail
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const DetailScreen = ({ navigation }) => {
+  const handleGoBack = () => navigation.goBack();
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Detail Screen</Text>
+
+      <Text onPress={handleGoBack}>Go Back</Text>
+    </SafeAreaView>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    margin: 20
-  }
-})
+  container: {
+    flex: 1,
+    margin: 20,
+  },
+});
+
--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,34 @@
-const { requireNativeComponent } = require("react-native");
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image} from 'react-native'
+export default class Header extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}> Weather Forecast</Text>
 
-export default class Header extends Component{
-    render(){
-        return(
-            <View>
-                <Text style={styles.text}> Weather Forecast</Text>
-                <Image source={require('../assets/sunny.png')} style={styles.img} />
-                <Text style={styles.text}>30°C</Text>
-                <Text style={styles.text}>Kathmandu</Text>
-            </View>
-        )
-    }
+        <Image source={require("../assets/sunny.png")} style={styles.img} />
+
+        <Text style={styles.text}>30°C</Text>
+
+        <Text style={styles.text}>Kathmandu</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-        text:{
-            fontSize: 28,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            color:'#333'
+  text: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#333",
+  },
+  img: {
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+    marginVertical: 20,
+  },
+});
 
-        },
-        img:{
-            width: 300,
-            height:300,
-            alignSelf:'center',
-            
-        }
-})
